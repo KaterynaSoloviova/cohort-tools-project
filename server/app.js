8f67625a0dc0cdb675fd7166947daa85990517bc
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2025-07-28 16:44:27 +0200

    feat: configure routes, add cors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
+const cors = require("cors");
+
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -20,6 +24,12 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
+
+
+
+
+
 
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
@@ -28,6 +38,15 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+app.get("/api/cohorts", (reg, res) => {
+  res.json(cohorts);
+});
+
+app.get("/api/students", (reg, res) => {
+  res.json(students);
+})
+
 
 
 // START SERVER
