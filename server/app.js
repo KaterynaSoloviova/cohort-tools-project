b7d381f4cc8b3dd46cee83d51f93883646629c3d
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2025-07-29 15:53:39 +0200

    feat: set up MongoDB with Compass, imported cohorts and students data, added Mongoose models and DB connection

--- server/app.js
+++ server/app.js
@@ -2,34 +2,35 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-const cohorts = require("./cohorts.json");
-const students = require("./students.json");
-const cors = require("cors");
+const Cohort = require("./models/Cohort.model");
+const Student = require("./models/Student.model");
 
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
-
-
-
-
-
 
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
@@ -39,15 +40,18 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
-app.get("/api/cohorts", (reg, res) => {
-  res.json(cohorts);
+app.get("/api/cohorts", (req, res) => {
+  Cohort.find()
+    .then(cohorts => res.json(cohorts))
+    .catch(err => res.status(500).json({ error: "Failed to fetch cohorts" }));
 });
 
-app.get("/api/students", (reg, res) => {
-  res.json(students);
-})
-
-
+app.get("/api/students", (req, res) => {
+  Student.find()
+    .populate("cohort")
+    .then(students => res.json(students))
+    .catch(err => res.status(500).json({ error: "Failed to fetch students" }));
+});
 
 // START SERVER
 app.listen(PORT, () => {
